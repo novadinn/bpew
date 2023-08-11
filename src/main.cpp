2fd87960bb83764289df87c6e05486a343add151
novadinn <80194776+novadinn@users.noreply.github.com>
2023-08-11 06:26:05 +0300

    Adjusted default imgui styles

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,67 @@ int main(int argc, char **argv) {
   // SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
   // SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, NUM_SAMPLES);
 
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  (void)io;
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+  io.ConfigDockingWithShift = true;
+
+  /* adjust the style */
+  ImGuiStyle &style = ImGui::GetStyle();
+  style.FrameRounding = 6.0f;
+  style.WindowBorderSize = 0.0f;
+  style.FrameBorderSize = 0.0f;
+  style.ChildBorderSize = 0.0f;
+  style.PopupBorderSize = 0.0f;
+  style.WindowRounding = 6.0f;
+  style.ChildRounding = 6.0f;
+  style.PopupRounding = 6.0f;
+  style.ScrollbarRounding = 6.0f;
+  style.GrabRounding = 6.0f;
+  style.TabRounding = 6.0f;
+
+  ImVec4 *colors = style.Colors;
+  colors[ImGuiCol_WindowBg] = ImVec4(0.28f, 0.28f, 0.28f, 1.00f);
+  colors[ImGuiCol_PopupBg] = ImVec4(0.39f, 0.39f, 0.39f, 1.00f);
+  colors[ImGuiCol_FrameBg] = ImVec4(0.19f, 0.19f, 0.19f, 1.00f);
+  colors[ImGuiCol_FrameBgHovered] = ImVec4(0.36f, 0.36f, 0.36f, 1.00f);
+  colors[ImGuiCol_FrameBgActive] = ImVec4(0.49f, 0.49f, 0.49f, 1.00f);
+  colors[ImGuiCol_TitleBg] = ImVec4(0.20f, 0.20f, 0.20f, 1.00f);
+  colors[ImGuiCol_TitleBgActive] = ImVec4(0.18f, 0.18f, 0.18f, 1.00f);
+  colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.18f, 0.18f, 0.18f, 1.00f);
+  colors[ImGuiCol_MenuBarBg] = ImVec4(0.18f, 0.18f, 0.18f, 1.00f);
+  colors[ImGuiCol_ScrollbarBg] = ImVec4(0.19f, 0.19f, 0.19f, 1.00f);
+  colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.55f, 0.55f, 0.55f, 1.00f);
+  colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.61f, 0.61f, 0.61f, 1.00f);
+  colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.71f, 0.71f, 0.71f, 1.00f);
+  colors[ImGuiCol_CheckMark] = ImVec4(0.81f, 0.27f, 0.04f, 1.00f);
+  colors[ImGuiCol_SliderGrab] = ImVec4(0.81f, 0.27f, 0.04f, 1.00f);
+  colors[ImGuiCol_SliderGrabActive] = ImVec4(0.91f, 0.28f, 0.02f, 1.00f);
+  colors[ImGuiCol_Button] = ImVec4(0.81f, 0.27f, 0.04f, 1.00f);
+  colors[ImGuiCol_ButtonHovered] = ImVec4(0.87f, 0.31f, 0.06f, 1.00f);
+  colors[ImGuiCol_ButtonActive] = ImVec4(1.00f, 0.33f, 0.05f, 1.00f);
+  colors[ImGuiCol_Header] = ImVec4(0.81f, 0.27f, 0.04f, 1.00f);
+  colors[ImGuiCol_HeaderHovered] = ImVec4(0.87f, 0.29f, 0.04f, 1.00f);
+  colors[ImGuiCol_HeaderActive] = ImVec4(0.94f, 0.33f, 0.07f, 1.00f);
+  colors[ImGuiCol_Separator] = ImVec4(0.75f, 0.75f, 0.75f, 0.50f);
+  colors[ImGuiCol_SeparatorHovered] = ImVec4(0.88f, 0.88f, 0.88f, 1.00f);
+  colors[ImGuiCol_SeparatorActive] = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+  colors[ImGuiCol_ResizeGrip] = ImVec4(0.81f, 0.27f, 0.04f, 1.00f);
+  colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.87f, 0.29f, 0.04f, 1.00f);
+  colors[ImGuiCol_ResizeGripActive] = ImVec4(0.93f, 0.31f, 0.05f, 1.00f);
+  colors[ImGuiCol_Tab] = ImVec4(0.30f, 0.30f, 0.30f, 1.00f);
+  colors[ImGuiCol_TabHovered] = ImVec4(0.40f, 0.40f, 0.40f, 0.80f);
+  colors[ImGuiCol_TabActive] = ImVec4(0.60f, 0.60f, 0.60f, 1.00f);
+  colors[ImGuiCol_TabUnfocused] = ImVec4(0.07f, 0.10f, 0.15f, 1.00f);
+  colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.43f, 0.43f, 0.44f, 1.00f);
+  colors[ImGuiCol_DockingPreview] = ImVec4(0.81f, 0.27f, 0.04f, 1.00f);
+  colors[ImGuiCol_DockingEmptyBg] = ImVec4(0.87f, 0.30f, 0.05f, 1.00f);
+  colors[ImGuiCol_TextSelectedBg] = ImVec4(0.81f, 0.27f, 0.04f, 1.00f);
+  colors[ImGuiCol_DragDropTarget] = ImVec4(1.00f, 0.30f, 0.00f, 1.00f);
+  colors[ImGuiCol_NavHighlight] = ImVec4(0.91f, 0.28f, 0.02f, 1.00f);
+
   Window *window = new Window();
   if (!window->create("BPew", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                       WINDOW_WIDTH, WINDOW_HEIGHT,
@@ -62,13 +123,6 @@ int main(int argc, char **argv) {
   if (!context) {
     exit(1);
   }
-
-  IMGUI_CHECKVERSION();
-  ImGui::CreateContext();
-  ImGuiIO &io = ImGui::GetIO();
-  (void)io;
-  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-  io.ConfigDockingWithShift = true;
 
   SDL_Window *native_window = window->getNativeWindow();
   ImGui_ImplSDL2_InitForOpenGL(native_window, context);
